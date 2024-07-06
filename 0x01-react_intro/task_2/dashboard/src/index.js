867a1c3cb8d7c17f7538c2fbb4391ffecae126e1
Modeste74 <ciiramodeste23@gmail.com>
2024-07-06 21:45:33 +0300

    contains the additional notification elements for the webpage.

--- 0x01-react_intro/task_2/dashboard/src/index.js
+++ 0x01-react_intro/task_2/dashboard/src/index.js
@@ -11,12 +11,11 @@ root.render(
       <div id="root-notifications">
         <Notifications />
       </div>
-      <div id="root">
+      <div id="root-content">
         <App />
       </div>
     </div>
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
